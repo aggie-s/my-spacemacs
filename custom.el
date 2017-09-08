db02ed81d379f23c57dee1ef320788a4241330e6
dehong song <dehongsong321@gmail.com>
2017-09-08 00:40:57 -0700

    init spacemacs config

---
+++ custom.el
@@ -0,0 +1,70 @@
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahs-case-fold-search nil t)
+ '(command-log-mode-window-size 50)
+ '(company-dabbrev-minimum-length 3)
+ '(company-dabbrev-other-buffers nil)
+ '(company-show-numbers t)
+ '(company-statistics-auto-restore nil)
+ '(ctags-update-delay-seconds 1024)
+ '(erc-nick "dehong")
+ '(erc-port 6666)
+ '(evil-want-C-i-jump t)
+ '(evil-want-Y-yank-to-eol nil)
+ '(exec-path-from-shell-arguments (quote ("-l")))
+ '(expand-region-contract-fast-key "V")
+ '(expand-region-exclude-text-mode-expansions (quote (html-mode nxml-mode web-mode)))
+ '(expand-region-reset-fast-key "r")
+ '(global-command-log-mode nil)
+ '(helm-buffer-max-length 56)
+ '(helm-move-to-line-cycle-in-source t)
+ '(ivy-height 18)
+ '(only-global-abbrevs t)
+ '(org-agenda-custom-commands nil)
+ '(org-agenda-ndays 1)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-deadline-prewarning-if-scheduled t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-start-on-weekday nil)
+ '(org-agenda-text-search-extra-files (quote (agenda-archives)))
+ '(org-deadline-warning-days 14)
+ '(org-fast-tag-selection-single-key (quote expert))
+ '(org-log-into-drawer t)
+ '(org-pomodoro-play-sounds nil)
+ '(org-reverse-note-order t)
+ '(package-selected-packages
+   (quote
+    (smartparens projectile parent-mode flx iedit anzu evil goto-chg undo-tree popup diminish counsel swiper ivy cmake-ide levenshtein hydra highlight spinner pkg-info epl bind-map bind-key packed async f dash s avy yapfify web-mode web-beautify unfill tide typescript-mode tagedit slim-mode scss-mode sayid sass-mode ranger rainbow-mode rainbow-identifiers pyvenv pytest pyenv-mode py-isort pug-mode pip-requirements origami org-pomodoro alert log4e gntp org-brain mwim live-py-mode less-css-mode json-mode json-snatcher json-reformat js2-refactor js2-mode js-doc impatient-mode simple-httpd hy-mode htmlize haml-mode gnuplot ggtags fuzzy flyspell-correct-ivy flyspell-correct flycheck-pos-tip pos-tip flycheck evil-org emmet-mode deft cython-mode company-web web-completion-data company-tern dash-functional tern company-statistics company-c-headers company-anaconda company color-identifiers-mode coffee-mode cmake-mode clojure-snippets clj-refactor inflections edn multiple-cursors paredit peg cider-eval-sexp-fu cider seq queue clojure-mode auto-yasnippet yasnippet anaconda-mode pythonic helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-flx helm-descbinds helm-ag ace-jump-helm-line ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org symon string-inflection spaceline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el password-generator paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-purpose ivy-hydra info+ indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-make google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav editorconfig dumb-jump define-word counsel-projectile column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link)))
+ '(paradox-github-token t)
+ '(ring-bell-function (quote ignore))
+ '(sp-show-pair-from-inside t)
+ '(tags-revert-without-query t)
+ '(vc-follow-symlinks t)
+ '(web-mode-markup-indent-offset 2)
+ '(ycmd-extra-conf-handler (quote load)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahs-definition-face ((t (:foreground "#d33682" :slant normal :weight bold))))
+ '(ahs-face ((t (:foreground "#d33682" :weight bold))))
+ '(command-log-command ((t (:foreground "dark magenta"))))
+ '(command-log-key ((t (:foreground "dark cyan"))))
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
+ '(iedit-occurrence ((t (:inherit cursor))))
+ '(ivy-virtual ((t (:background "skyblue"))))
+ '(js2-external-variable ((t (:foreground "plum3"))))
+ '(mc/cursor-bar-face ((t (:background "chartreuse3"))))
+ '(show-paren-match ((t (:background "dark gray" :foreground "#d33682" :weight bold))))
+ '(sp-show-pair-match-face ((t (:background "#272822" :foreground "gray" :inverse-video t :weight normal))))
+ '(web-mode-current-element-highlight-face ((t (:background "dark gray")))))
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
